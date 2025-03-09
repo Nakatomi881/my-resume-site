67045e068780878b8e2bb6679aa2912f2e0a2724
Ryan Palmer <ryandanpalmer@gmail.com>
2025-03-09 17:45:16 -0600

    add react-router-dom

--- my-resume-site/src/App.tsx
+++ my-resume-site/src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Resume from './pages/resume/resume'
 import Navbar from './components/navbar/Navbar'
-import About from './pages/resume/about/About'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
 
 function App() {
   return (
     <>
       <Navbar />
-      <About />
+      <Routes>
+      <Route path="/" element={<Resume />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   )
 }
--- my-resume-site/src/pages/resume/resume.tsx
+++ my-resume-site/src/pages/resume/resume.tsx
@@ -4,7 +4,10 @@ import styles from './resume.module.css'
 const Resume: React.FC = () => {
   return (
       <div className={styles.container}>
-        
+        <div className={styles.heading}>
+          <h1>Ryan Palmer</h1>
+          <p>Helping companies improve product quality for their users</p>
+        </div>
       </div>
     );
 };
