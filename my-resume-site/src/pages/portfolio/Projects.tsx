da2566e7e04903b25b9edab559742c5a0153df40
Ryan Palmer <ryandanpalmer@gmail.com>
2025-03-16 16:24:13 -0600

    added portfolio folder

---
+++ my-resume-site/src/pages/portfolio/Projects.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './projects.module.css'
+
+const Projects: React.FC = () => {
+  return (
+      <div className={styles.container}>
+        
+      </div>
+    );
+};
+
+
+export default Projects;
