5cca1cd8ea8eb1157ec5d51926dd4f816e10cd0d
Ryan Palmer <ryandanpalmer@gmail.com>
2025-04-05 19:18:44 -0600

    update contact page

--- my-resume-site/src/pages/contact/Contact.tsx
+++ my-resume-site/src/pages/contact/Contact.tsx
@@ -1,13 +1,43 @@
 import React from "react";
-import styles from './contact.module.css'
+import styles from './contact.module.css';
 
 const Contact: React.FC = () => {
   return (
+    <div className={styles.contactSection}>
+      <h1 className={styles.heading}>Contact</h1>
+      
       <div className={styles.container}>
-        
-      </div>
-    );
-};
+        <div className={styles.left}>
+          <div className={styles.email}>
+            <p>ryandanpalmer@gmail.com</p>
+          </div>
+          <div className={styles.location}>
+            <p>Utah</p>
+          </div>
+        </div>
 
+        <div className={styles.right}>
+          <form action="https://formspree.io/f/xovepana" method="POST">
+            <div className={styles.yourname}>
+              <label htmlFor="name">Your Name</label>
+              <input placeholder="John McClane" id="name" required type="text" />
+            </div>
+            <div>
+              <label htmlFor="email">Your email</label>
+              <input placeholder="Youremail@example.com" id="email" required type="text" />
+            </div>
+            <div>
+              <label htmlFor="textarea">Your message</label>
+              <textarea id="textarea" name="message" required placeholder="Type your message here" rows={5} />
+            </div>
+            <div>
+              <input type="submit" id="button" value="Send" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
