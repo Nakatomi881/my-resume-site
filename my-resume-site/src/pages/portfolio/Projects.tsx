ea93e7148af0f188e6ef73618757fb018274c212
Ryan Palmer <ryandanpalmer@gmail.com>
2025-04-26 14:09:42 -0600

    updated projects page, navbar css fixes

--- my-resume-site/src/pages/portfolio/Projects.tsx
+++ my-resume-site/src/pages/portfolio/Projects.tsx
@@ -4,7 +4,10 @@ import styles from './projects.module.css'
 const Projects: React.FC = () => {
   return (
       <div className={styles.container}>
-        
+        <h1 className={styles.heading}>Projects</h1>
+        <div>
+          <p>Coming soon</p>
+        </div>
       </div>
     );
 };
