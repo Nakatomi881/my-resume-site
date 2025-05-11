1b72ae0be154d56d397c43b76dec93c11b21799e
Ryan Palmer <ryandanpalmer@gmail.com>
2025-05-11 12:10:43 -0600

    update projects

--- my-resume-site/src/pages/portfolio/Projects.tsx
+++ my-resume-site/src/pages/portfolio/Projects.tsx
@@ -5,8 +5,20 @@ const Projects: React.FC = () => {
   return (
       <div className={styles.container}>
         <h1 className={styles.heading}>Projects</h1>
-        <div>
-          <p>Coming soon</p>
+        <div className={styles.grid}>
+         
+
+          <div className={styles.card}>
+            <p>Brinkerhoff Drywall is a construction company based in Spanish Fork, UT, specializing in Wood Framing</p>
+          </div>
+
+          <div className={styles.card}>
+            <p>Playwright testing for Palmer Software Consulting</p>
+          </div>
+
+          <div className={styles.card}>
+            <p>KofM technology helping with quick websites and easy testing </p>
+          </div>
         </div>
 
         
